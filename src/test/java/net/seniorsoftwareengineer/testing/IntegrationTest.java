656d80555044b78281fc64b8d960148ee2c71909
Andrea <a.pag1@yahoo.it>
2022-09-07 01:26:55 +0200

    Update JUnit usecase test

--- src/test/java/net/seniorsoftwareengineer/testing/IntegrationTest.java
+++ src/test/java/net/seniorsoftwareengineer/testing/IntegrationTest.java
@@ -5,9 +5,6 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -18,7 +15,6 @@ import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
-import net.seniorsoftwareengineer.testing.activity.Activity;
 import net.seniorsoftwareengineer.testing.activity.Click;
 import net.seniorsoftwareengineer.testing.activity.Exist;
 import net.seniorsoftwareengineer.testing.activity.ExistText;
@@ -84,89 +80,70 @@ public class IntegrationTest {
 
     @Test
     public void PageTestingRest_urlNull() throws Exception {
-	message.getConfiguration().setBrowserVersion(testProperty.getBrowserVersion());
-	mvc.perform(get(testProperty.getApiTest()).content(JsonUtils.convertObjectToJsonString(message))
+	Configuration configuration = BuilderConfiguration.create().browserVersion(testProperty.getBrowserVersion())
+		.driverVersion(testProperty.getDriverVersion()).getConfiguration();
+	TestCase test = BuilderTestCase.create().configuration(configuration).getTestCase();
+	mvc.perform(get(testProperty.getApiTest()).content(JsonUtils.convertObjectToJsonString(test))
 		.contentType(testProperty.getApiContentType())).andExpect(status().is5xxServerError())
 		.andExpect(jsonPath("$.['errors']['url']['text']", isA(String.class)));
     }
 
     @Test
     public void PageTestingRest_testsimple() throws Exception {
-	message.getConfiguration().setBrowserVersion(testProperty.getBrowserVersion());
-	message.setUrl(testProperty.getUrl());
-	mvc.perform(get(testProperty.getApiTest()).content(JsonUtils.convertObjectToJsonString(message))
+	Click click = new Click();
+	click.getSelector().setCssSelector("body");
+
+	Configuration configuration = BuilderConfiguration.create().browserVersion(testProperty.getBrowserVersion())
+		.driverVersion(testProperty.getDriverVersion()).getConfiguration();
+	TestCase test = BuilderTestCase.create().url(testProperty.getUrl()).addActivity(click)
+		.configuration(configuration).getTestCase();
+	mvc.perform(get(testProperty.getApiTest()).content(JsonUtils.convertObjectToJsonString(test))
 		.contentType(testProperty.getApiContentType())).andExpect(status().is(200));
     }
 
     @Test
     public void PageTestingRest_textToInsert() throws Exception {
-	message.getConfiguration().setBrowserVersion(testProperty.getBrowserVersion());
-	message.getConfiguration().setUseJQuery(true);
-	List<Activity> list = new ArrayList<Activity>();
+
 	InsertText insertText = new InsertText();
 	insertText.setType("InsertText");
 	((InsertText) insertText).setTextToInsert("test");
 	insertText.getSelector().setCssSelector("[name='q']");
-	message.getElementDOM().getSelector().setCssSelector("[name='q']");
 
-	list.add(insertText);
-
-	message.setActivities(list);
-	message.setUrl(testProperty.getUrl());
-	mvc.perform(get(testProperty.getApiTest()).content(JsonUtils.convertObjectToJsonString(message))
+	Configuration configuration = BuilderConfiguration.create().browserVersion(testProperty.getBrowserVersion())
+		.driverVersion(testProperty.getDriverVersion()).getConfiguration();
+	TestCase test = BuilderTestCase.create().configuration(configuration).addActivity(insertText)
+		.url(testProperty.getUrl()).getTestCase();
+	mvc.perform(get(testProperty.getApiTest()).content(JsonUtils.convertObjectToJsonString(test))
 		.contentType(testProperty.getApiContentType())).andExpect(status().is(500));
     }
 
     @Test
-    public void PageTestingRest_click() throws Exception {
-	message.getConfiguration().setBrowserVersion(testProperty.getBrowserVersion());
-	List<Activity> list = new ArrayList<Activity>();
-
-	Click click = new Click();
-	click.setType("Click");
-	click.getSelector().setCssSelector("[name='q']");
-	message.getElementDOM().getSelector().setCssSelector("[name='q']");
-
-	list.add(click);
-
-	message.setActivities(list);
-	message.setUrl(testProperty.getUrl());
-	mvc.perform(get(testProperty.getApiTest()).content(JsonUtils.convertObjectToJsonString(message))
-		.contentType(testProperty.getApiContentType())).andExpect(status().is(200));
-    }
-
-    @Test
     public void PageTestingRest_exist() throws Exception {
-	message.getConfiguration().setBrowserVersion(testProperty.getBrowserVersion());
-	List<Activity> list = new ArrayList<Activity>();
-
 	Exist exist = new Exist();
 	exist.setType("Exist");
 	exist.getSelector().setCssSelector("[name='q']");
 	message.getElementDOM().getSelector().setCssSelector("[name='q']");
-	list.add(exist);
 
-	message.setActivities(list);
-	message.setUrl(testProperty.getUrl());
-	mvc.perform(get(testProperty.getApiTest()).content(JsonUtils.convertObjectToJsonString(message))
+	Configuration configuration = BuilderConfiguration.create().browserVersion(testProperty.getBrowserVersion())
+		.driverVersion(testProperty.getDriverVersion()).getConfiguration();
+	TestCase test = BuilderTestCase.create().configuration(configuration).addActivity(exist)
+		.url(testProperty.getUrl()).getTestCase();
+	mvc.perform(get(testProperty.getApiTest()).content(JsonUtils.convertObjectToJsonString(test))
 		.contentType(testProperty.getApiContentType())).andExpect(status().is(200));
     }
 
     @Test
     public void PageTestingRest_existText() throws Exception {
-	message.getConfiguration().setBrowserVersion(testProperty.getBrowserVersion());
-	List<Activity> list = new ArrayList<Activity>();
-
 	ExistText exist = new ExistText();
 	exist.setType("ExistText");
 	exist.getSelector().setCssSelector("[name='q']");
-	message.getElementDOM().getSelector().setCssSelector("[name='q']");
-	list.add(exist);
 
-	message.setActivities(list);
-	message.setUrl(testProperty.getUrl());
-	mvc.perform(get("/test").content(JsonUtils.convertObjectToJsonString(message)).contentType("application/json"))
-		.andExpect(status().is(200));
+	Configuration configuration = BuilderConfiguration.create().browserVersion(testProperty.getBrowserVersion())
+		.driverVersion(testProperty.getDriverVersion()).getConfiguration();
+	TestCase test = BuilderTestCase.create().configuration(configuration).addActivity(exist)
+		.url(testProperty.getUrl()).getTestCase();
+	mvc.perform(get(testProperty.getApiTest()).content(JsonUtils.convertObjectToJsonString(test))
+		.contentType(testProperty.getApiContentType())).andExpect(status().is(200));
     }
 
 }
